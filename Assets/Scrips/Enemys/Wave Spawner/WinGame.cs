88f5837142ea80ec06199925088e222abb0b548a
Malte747 <malte.lehmann@students.hs-mainz.de>
2023-11-23 18:43:05 +0100

    Fixed Stuff
    Fixed Shit

--- Assets/Scrips/Enemys/Wave Spawner/WinGame.cs
+++ Assets/Scrips/Enemys/Wave Spawner/WinGame.cs
@@ -5,24 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class WinGame : MonoBehaviour
 {
-    public int next = 1;
+    MainMenu _mainmenu;
+    WaveSpawner WaveSpawner;
+    WaveSpawner2 WaveSpawner2;
+    WaveSpawner3 WaveSpawner3;
+    public bool GameOver = false;
+
+    void Start() {
+        {
+      
+        _mainmenu = GameObject.Find("LevelManager").GetComponent<MainMenu>();
+        WaveSpawner = GetComponent<WaveSpawner>();
+        WaveSpawner2 = GetComponent<WaveSpawner2>();
+        WaveSpawner3 = GetComponent<WaveSpawner3>();
+        }
+    }
     void Update()
     {
-        WaveSpawner WaveSpawner = GetComponent<WaveSpawner>();
-        WaveSpawner2 WaveSpawner2 = GetComponent<WaveSpawner2>();
-        WaveSpawner3 WaveSpawner3 = GetComponent<WaveSpawner3>();
-        MainMenu levelCounter = GetComponent<MainMenu>();
       
 
-        if(WaveSpawner.WinActive && WaveSpawner2.WinActive && WaveSpawner3.WinActive)
+        if(WaveSpawner.WinActive && WaveSpawner2.WinActive && WaveSpawner3.WinActive && !GameOver)
         {
             Win();
-            levelCounter.LevelCounter();
+            
         }
     }
 
     void Win()
-    {
+    {      
+        GameOver = true;
+          _mainmenu.LevelCounter();
           Debug.Log("Win");
           SceneManager.LoadSceneAsync(2);
     }
